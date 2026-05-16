2e65e34cdda12bead6288dc59f22708933c314ae
choco-melk <154525691+choco-melk@users.noreply.github.com>
2026-05-17 07:39:26 +0800

    [GREEN] Implement integration layer for Note Fetching

--- backend/index.js
+++ backend/index.js
@@ -16,6 +16,24 @@ app.get("/notes", (req, res) => {
     }
 })
 
+app.get("/notes/:id", (req, res) => {
+    try {
+        const id = parseInt(req.params.id, 10);
+        if (Number.isNaN(id) || id < 0) {
+            return res.status(400).send({ "error": "Invalid ID format" });
+        }
+
+        if (notesDB[id]) {
+            res.status(200).send(notesDB[id]);
+        } else {
+            res.status(404).send({ "error": "A note with this ID does not exist." });
+        }
+        
+    } catch (error) {
+        res.status(500).send({ "error": error.message });
+    }
+});
+
 app.post("/notes", (req, res) => {
     try {
         const {title, content} = req.body;
--- src/utils/getNote.js
+++ src/utils/getNote.js
@@ -1,3 +1,17 @@
 export default async function getNote(id) {
-    return "Not implemented";
+    try {
+        const endpoint = `http://localhost:3000/notes/${id}`;
+        const request = {
+            "method": "GET",
+        };
+        const response = await fetch(endpoint, request);
+        const result = await response.json();
+        console.log(response.status);
+        return {"status": response.status, "body": result};    
+    } catch(e) {
+        return { 
+            "status": 500, 
+            "body": { "error": "Unable to retrieve a note at this time" } 
+        };
+    }
 }
