49feedbfa40e94b1d011f066b1e713f1aadadb25
choco-melk <154525691+choco-melk@users.noreply.github.com>
2026-05-17 01:09:04 +0800

    [GREEN] Implement integration layer for Note Editting

--- backend/index.js
+++ backend/index.js
@@ -26,6 +26,22 @@ app.post("/notes", (req, res) => {
     }
 });
 
+app.put("/notes/:id", (req, res) => {
+    try {
+        const id = parseInt(req.params.id, 10);
+        const result = req.body;
+        if (notesDB[id]) {
+            notesDB[id] = {...result, "id": id};
+            res.status(200).send(notesDB[id]);
+        } else {
+            res.status(404).send({ "error": "Note ID not found" });
+        }
+
+    } catch (error) {
+        res.status(500).send({"error" : error.message})
+    }
+});
+
 app.listen(3000, () => {
     console.log("Server is running in http://localhost:3000")
 });
--- src/utils/editNote.js
+++ src/utils/editNote.js
@@ -1,3 +1,16 @@
 export default async function editNode(id, note) {
-    return "Not Implemented";
+    try {
+        const endpoint = `http://localhost:3000/notes/${id}`;
+        const request = {
+            "method": "PUT",
+            "headers": {"content-type": "application/json"},
+            "body": JSON.stringify(note)
+        };
+        const response = await fetch(endpoint, request);
+        const result = await response.json();
+        return {"status": response.status, "body": result};    
+    } catch(e) {
+        return e.message;
+    }
+  
 } 
